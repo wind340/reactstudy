06d4cdeec596acb228c7382e91f66572d60ec519
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-13 23:45:46 +0900

    webpack 설정

---
+++ react-webgame/lecture/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  name: "wordrealy-setting",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  entry: {
+    app: ["./client"],
+  }, //입력
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  }, //출력
+};
