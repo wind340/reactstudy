c03c874cebba122de6c13c79522c66994c70da01
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-16 17:13:25 +0900

    끝말잇기로 전반적인 복습

---
+++ react-webgame/2.끝말잇기/WordRelay.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState, useRef } from "react";
+
+const WordRelay = () => {
+  const [word, setWord] = useState("기찻길");
+  const [result, setResult] = useState("");
+  const inputRef = useRef(null);
+
+  const onSubmitForm = e => {
+    e.preventDefault();
+    console.dir(e.target);
+    console.log(e.target.children.word.value);
+    if (word[word.length - 1] === e.target.children.word.value[0]) {
+      setResult("딩동댕"),
+        setWord(e.target.children.word.value),
+        (e.target.children.word.value = "");
+      inputRef.current.focus();
+    } else {
+      setResult("땡");
+      e.target.children.word.value = "";
+      inputRef.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>{word}</div>
+      <form onSubmit={onSubmitForm}>
+        <input defaultValue="기찻길" id="word" ref={inputRef} />
+        <button>입력!</button>
+      </form>
+      <div>{result}</div>
+    </>
+  );
+};
+
+export default WordRelay;
---
+++ react-webgame/2.끝말잇기/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+import WordRelay from "./WordRelay";
+
+render(<WordRelay />, document.querySelector("#root"));
