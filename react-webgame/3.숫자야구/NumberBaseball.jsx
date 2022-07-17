ab99bb1db195c942b071b545afbd5411e7e4bcd8
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-07-17 23:56:09 +0900

    숫자 야구 게임

---
+++ react-webgame/3.숫자야구/NumberBaseball.jsx
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import Try from "./try";
+
+function getNumbers() {
+  // 숫자 네개를 겹치지 않고 랜덤하게 뽑는 함수
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
+}
+
+class NumberBaseball extends Component {
+  state = {
+    result: "",
+    value: "",
+    answer: getNumbers(),
+    tries: [],
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.value === this.state.answer.join("")) {
+      this.setState({
+        result: "홈런!",
+        tries: [
+          ...this.state.tries,
+          { try: this.state.value, result: "홈런!" },
+        ],
+      });
+    } else {
+      const answerArray = this.state.value.split("").map(v => parseInt(v));
+      let strike = 0;
+      let ball = 0;
+      if (this.state.tries.length >= 9) {
+        // 10번이내 실패시
+        this.setState({
+          result: `10번 넘게 돌려서 실패! 답은 ${answer.join("")} 였습니다.`,
+        });
+        alert("게임을 다시 시작합니다");
+        this.setState({
+          value: "",
+          answer: getNumbers(),
+          tries: [],
+        });
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === this.state.answer[i]) {
+            strike += 1;
+          } else if (this.state.answer.includs(answerArray[i])) {
+            ball += 1;
+          }
+        }
+        this.setState({
+          tries: [
+            ...this.state.tries,
+            {
+              try: this.state.value,
+              result: `${strike} 스트라이크, ${ball}볼입니다`,
+            },
+          ],
+        });
+      }
+    }
+  };
+
+  onChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  render() {
+    return (
+      <>
+        <div>{this.state.result}</div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            maxLength={4}
+            onChange={this.onChange}
+            value={this.state.value}
+          ></input>
+          <button type="submit">입력</button>
+        </form>
+        <div>시도: {this.state.tries.length}</div>
+        <ul>
+          {this.state.tries.map((v, i) => {
+            return <Try key={`${i}차 시도" : `} tryInfo={v} />;
+          })}
+        </ul>
+        <div>{this.state.result}</div>
+      </>
+    );
+  }
+}
+
+export default NumberBaseball;
---
+++ react-webgame/3.숫자야구/client.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { render } from "react-dom";
+import NumberBaseball from "./NumberBaseball";
+
+render(<NumberBaseball />, document.querySelector("#root"));
